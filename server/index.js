ff3b355bcc3c1d0323df7b5efb83d253cf24857e
cctina516 <christinayan1019@gmail.com>
2021-04-25 18:11:51 -0700

    socketio server side

--- server/index.js
+++ server/index.js
@@ -5,9 +5,23 @@ const http = require('http');
 //fist part for deloy later
 const PORT = process.env.PORT || 5000;
 
+//get router from router file
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) =>{
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () =>{
+        console.log('User left!!!');
+    })
+});
+
+//call router as a middleware
+app.use(router);
 
 //run the server
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
